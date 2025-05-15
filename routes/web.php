496f46fe96c532ee9d7134b54546d18acf4d2a7b
yessir <tdevelopeer@gmail.com>
2025-05-15 15:09:18 +0300

    Роуты с городами

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/reset', function () {
+    session()->forget('city');
+    return redirect()->route('index');
+})->name('reset');
+
+Route::prefix('{city?}')->group(function () {
+    Route::get('/', [MainController::class, 'index'])->name('index');
+    Route::get('/about', [MainController::class, 'about'])->name('about');
+    Route::get('/news', [MainController::class, 'news'])->name('news');
 });
+
